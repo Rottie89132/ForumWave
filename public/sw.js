8d4528391bf0781241f114d0d2b5489d96417cb7
Rottie#7658 <89132@glr.nl>
2024-06-20 18:30:07 +0200

    api chaching toegevoegd

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,27 @@
 import { clientsClaim } from "workbox-core";
 import { precacheAndRoute, cleanupOutdatedCaches } from "workbox-precaching";
+import { registerRoute } from 'workbox-routing';
+import { NetworkFirst } from 'workbox-strategies';
+
 
 self.skipWaiting();
 clientsClaim();
 precacheAndRoute(self.__WB_MANIFEST);
 cleanupOutdatedCaches();
 
+registerRoute(
+    ({ url }) => {
+        const pathSegments = url.pathname.split('/');
+        return pathSegments.length >= 3 && pathSegments[1] === 'api';
+    },
+    new NetworkFirst({
+        cacheName: 'api-cache',
+        plugins: [
+            {
+                cacheableResponse: {
+                    statuses: [0, 200],
+                }
+            }
+        ]
+    })
+);
